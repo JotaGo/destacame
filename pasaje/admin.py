ed887e43e2fb10ea2ad4403b01623d2996d87976
Juan Marquez <jemarqueza1993@hotmail.com>
2019-04-23 19:09:15 -0400

    Modelado primera etapa

--- pasaje/admin.py
+++ pasaje/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+admin.site.register(Trayecto)
+admin.site.register(Pasajero)
+admin.site.register(Asiento)
+admin.site.register(Bus)
+admin.site.register(Chofer)
--- pasaje/models.py
+++ pasaje/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Pasajero(models.Model):
+    run = models.CharField(max_length=13)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+
+class Chofer(models.Model):
+    run = models.CharField(max_length=13)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+
+class Trayecto(models.Model):
+    nombre = models.CharField(max_length=30)
+    horario = models.TimeField(auto_now=False, auto_now_add=False, null=False, blank=False)
+    pasajeros = models.ManyToManyField(Pasajero, through='Asiento')
+
+class Asiento(models.Model):
+    trayectos = models.ForeignKey(Trayecto, on_delete=models.CASCADE)
+    pasajeros = models.ForeignKey(Pasajero, on_delete=models.CASCADE)
+    asientos = models.PositiveSmallIntegerField(
+        unique=True,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+            ]
+        )
+
+class Bus(models.Model):
+    matricula = models.CharField(max_length=10)
+    piloto = models.OneToOneField(Chofer, null=True, blank=True, on_delete=models.SET_NULL)
+    trayectos = models.ForeignKey(Trayecto, null=False, blank=False, on_delete=models.CASCADE)
